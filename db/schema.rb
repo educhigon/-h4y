1fa19e379b9fd20d81234136dbafb9a23f0b0190
EDUARDO GONZALO <educhigon@gmail.com>
2024-05-28 14:45:37 +0200

    Few things

--- db/schema.rb
+++ db/schema.rb
@@ -36,8 +36,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_28_122043) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -76,6 +78,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_28_122043) do
   add_foreign_key "comments", "users"
   add_foreign_key "favorites", "posts"
   add_foreign_key "favorites", "users"
+  add_foreign_key "posts", "users"
   add_foreign_key "profiles", "users"
   add_foreign_key "reviews", "posts"
   add_foreign_key "reviews", "users"
