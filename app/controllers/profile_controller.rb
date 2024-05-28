5a8f8662fdba28a27496e8f27f1b947b89fc3ca8
Jeremy Hanhoff <jhanhoff@outlook.de>
2024-05-28 15:53:33 +0200

    Actions set-up for Profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,29 +1,34 @@
 class ProfileController < ApplicationController
 
-def show
-  @profile = Profile.new
-end
-
-def new
-  @profile = Profile.new
-end
-
-def create
-  @profile = Profile.new
-  @profile.user = current_user
-
-  if @profile.save
-    redirect_to profile_path(@profile)
-  else
-    render :new, status: :unprocessable_entity
+  def show
+    @profile = Profile.new
   end
-end
 
-def edit
-  @profile = Profile.find(params[:id])
-end
+  def new
+    @profile = Profile.new
+  end
 
-def update
-end
+  def create
+    @profile = Profile.new(profile_params)
+    @profile.user = current_user
 
+    if @profile.save
+      redirect_to profile_path(@profile)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @profile = Profile.find(params[:id])
+  end
+
+  def update
+    @profile = Profile.find(params[:id])
+    if @profile.update(profile_params)
+      redirect_to profile_path(@profile)
+    else
+      render :new, status: 422
+    end
+  end
 end
