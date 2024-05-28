4ee9c45ada929ac57c2814e0438cb7a6daa03069
Bruno Thormählen <brunothormaehlen@yahoo.com>
2024-05-28 16:32:51 +0200

    added setup for posts routes ; small adjustments to seed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :posts, only: [:index]
+  resources :posts, only: [:index, :show, :new, :create]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
