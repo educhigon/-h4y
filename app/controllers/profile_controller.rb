10d15e77e0f90bfbb1c1e699eb1ca556e27887b9
Jeremy Hanhoff <jhanhoff@outlook.de>
2024-05-28 15:41:34 +0200

    profile set-up in the works

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,29 @@
+class ProfileController < ApplicationController
+
+def show
+  @profile = Profile.new
+end
+
+def new
+  @profile = Profile.new
+end
+
+def create
+  @profile = Profile.new
+  @profile.user = current_user
+
+  if @profile.save
+    redirect_to profile_path(@profile)
+  else
+    render :new, status: :unprocessable_entity
+  end
+end
+
+def edit
+  @profile = Profile.find(params[:id])
+end
+
+def update
+end
+
+end
