8bf6d7e93c4e4f87bb2e6f507547d071113210d9
Jeremy Hanhoff <jhanhoff@outlook.de>
2024-06-03 16:51:03 +0200

    fixed index view for gallery function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,7 +45,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, tag_ids: [], :photos => [])
+    params.require(:post).permit(:title, :content, tag_ids: [], photos: [])
   end
 
   def create_new_tags(taggable)
