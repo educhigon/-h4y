46a535e2b399a956b34affd0062f316a1b8df3cd
Bruno Thormählen <brunothormaehlen@yahoo.com>
2024-06-05 11:33:13 +0200

    seed with images

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   # @route_prefix = request.path_parameters[:controller]
+  # in the `application_controller.rb`
+
 end
